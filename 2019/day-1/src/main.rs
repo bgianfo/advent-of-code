8fa92784a5f440b4c5c34431a571c9a315c24534
Brian Gianforcaro <b.gianfo@gmail.com>
2019-12-02 18:14:23 -0800

    :heavy_check_mark: Solution to 2019-day-1

--- 2019/day-1/src/main.rs
+++ 2019/day-1/src/main.rs
@@ -1,6 +1,6 @@
 //! Link: https://adventofcode.com/2019/day/1
 //!
-//!--- Day 1: The Tyranny of the Rocket Equation ---
+//! --- Day 1: The Tyranny of the Rocket Equation ---
 //! Santa has become stranded at the edge of the Solar System while delivering presents to other planets! To accurately calculate his position in space, safely align his warp drive, and return to Earth in time to save Christmas, he needs you to bring him measurements from fifty stars.
 //!
 //! Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
@@ -22,6 +22,21 @@
 //! To find it, individually calculate the fuel needed for the mass of each module (your puzzle input), then add together all the fuel values.
 //! What is the sum of the fuel requirements for all of the modules on your spacecraft?!
 //!
+//!
+//! Solution:
+//!
+//!  $ cargo run input.txt
+//!    Compiling aoc-2019-day-1 v0.1.0 (C:\src\advent-of-code\2019\day-1)
+//!     Finished dev [unoptimized + debuginfo] target(s) in 1.34s
+//!      Running `C:\src\advent-of-code\2019\day-1\target\debug\aoc-2019-day-1.exe input.txt`
+//!
+//!  Parsing file: input.txt
+//!  Total Fuel Required: 3443395
+//!  Success
+//!
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 /// "To find the fuel required for a module, take its mass, divide by three, round down, and subtract 2."
 fn calculate_fuel_required(mass: u64) -> u64 {
@@ -39,6 +54,39 @@ fn test_calculate_fuel_requried() {
     assert_eq!(33583, calculate_fuel_required(100756));
 }
 
+fn process_file(filename: &str) -> std::io::Result<()> {
+
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let mut accumulated_fuel: u64 = 0;
+
+    for line in reader.lines() {
+        let mass = line.unwrap().parse::<u64>().unwrap();
+        accumulated_fuel += calculate_fuel_required(mass);
+    }
+
+    // Print the accumulation.
+    //
+    println!("Total Fuel Required: {}", accumulated_fuel);
+
+    Ok(())
+}
+
 fn main() {
-    println!("TODO: Implement.");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+
+        let filename = &args[1];
+
+        println!("Parsing file: {}", filename);
+
+        match process_file(filename) {
+            Ok(_n) => println!("Success"),
+            Err(_e) => println!("Error"),
+        }
+    } else {
+        println!("Missing input file");
+    }
 }
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ My https://adventofcode.com solutions in rust.
 
 2019 Solutions:
 
-- [Day #1](2019/day-1) - https://adventofcode.com/2019/day/1
+- [Day #1](2019/day-1) - https://adventofcode.com/2019/day/1 :heavy_check_mark: 
 - [Day #2](2019/day-2) - https://adventofcode.com/2019/day/2
 - [Day #3](2019/day-4) - https://adventofcode.com/2019/day/3
 
